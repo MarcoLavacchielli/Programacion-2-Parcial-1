230ba4971c24de1a3a00a96145f400065b011a28
Lucas-1812 <lucasetorres2013@gmail.com>
2023-06-30 19:56:35 -0300

    cambio en fade

--- Assets/Scripts/inventoryObjectsActions.cs
+++ Assets/Scripts/inventoryObjectsActions.cs
@@ -84,7 +84,7 @@ public class inventoryObjectsActions : MonoBehaviour
         float alpha = 1;
         while (alpha >= 0)
         {
-            alpha -= 0.004f;
+            alpha -= 0.002f;
             yield return new WaitForEndOfFrame();
             newCardCanvasGroup.alpha = alpha;
         }
